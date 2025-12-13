7294f1366140508d42a11c52353ca546b22b97e2
boxrocket <splatterbiker@gmail.com>
2025-12-12 18:10:36 -0600

    use correct offset for scrubber in DoLayout too

--- game/addons/tools/Code/Widgets/SoundPlayer/SoundPlayer.cs
+++ game/addons/tools/Code/Widgets/SoundPlayer/SoundPlayer.cs
@@ -163,7 +163,7 @@ public partial class SoundPlayer : Widget
 			WaveForm.SceneRect = r;
 			WaveForm.Analyse();
 
-			Scrubber.Position = Scrubber.Position.WithX( PositionFromTime( Time ) - 3 ).SnapToGrid( 1.0f );
+			Scrubber.Position = Scrubber.Position.WithX( PositionFromTime( Time ) - 4 ).SnapToGrid( 1.0f );
 		}
 
 		protected override void OnResize()
