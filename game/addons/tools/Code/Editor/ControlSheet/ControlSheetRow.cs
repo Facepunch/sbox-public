976257bb269821821a969a9434ec4395548e00f4
boxrocket <69829964+johncosfm@users.noreply.github.com>
2026-01-13 13:49:22 -0600

    add apply to scene action for game object properties (for individual transform rows)

--- game/addons/tools/Code/Editor/ControlSheet/ControlSheetRow.cs
+++ game/addons/tools/Code/Editor/ControlSheet/ControlSheetRow.cs
@@ -284,6 +284,7 @@ class ControlSheetRow : Widget
 			menu.AddOption( $"Jump to code", "code", action: () => CodeEditor.OpenFile( property.SourceFile, property.SourceLine ) );
 		}
 
+		AddGameObjectOptions( menu );
 		AddComponentOptions( menu );
 
 		menu.OpenAt( e.ScreenPosition, false );
@@ -329,6 +330,41 @@ class ControlSheetRow : Widget
 		} );
 		setter.Enabled = Json.Serialize( prop.GetValue<object>() ) != Json.Serialize( property.GetValue<object>() );
 	}
+
+	void AddGameObjectOptions( Menu menu )
+	{
+		if ( !IsEditingGameObject )
+			return;
+
+		var gameobject = EditedGameObjects.FirstOrDefault();
+
+		// Only show if we're editing in a game session
+		var session = SceneEditorSession.Resolve( gameobject?.Scene );
+		if ( session is null || !session.IsPlaying )
+			return;
+
+		// try to find the version of this object in the editor session
+		var targetObject = session.Scene.Directory.FindByGuid( gameobject.Id );
+		if ( !targetObject.IsValid() ) return;
+
+		// get a serialized version of this object from that session
+		var so = targetObject.GetSerialized();
+		var prop = so.GetProperty( property.Name );
+
+		// add option to apply this value to that scene
+		menu.AddSeparator();
+		var setter = menu.AddOption( "Apply to Scene", "save", () =>
+		{
+			using var scope = session.Scene.Push();
+
+			using ( session.UndoScope( "Apply to Scene" ).WithGameObjectChanges( targetObject, GameObjectUndoFlags.Properties ).Push() )
+			{
+				prop.SetValue<object>( property.GetValue<object>() );
+			}
+		} );
+		setter.Enabled = Json.Serialize( prop.GetValue<object>() ) != Json.Serialize( property.GetValue<object>() );
+	}
+
 }
 
 internal class InfoBoxWidget : Widget
