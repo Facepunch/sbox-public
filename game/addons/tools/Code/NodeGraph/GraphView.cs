c125fd1c9b1a403f9e15de47a377b167ccb0eb0f
fishy <haydenwprice95@gmail.com>
2026-01-15 14:14:03 +1000

    Update GraphView.cs

--- game/addons/tools/Code/NodeGraph/GraphView.cs
+++ game/addons/tools/Code/NodeGraph/GraphView.cs
@@ -1185,8 +1185,11 @@ public class GraphView : GraphicsView, IGridSizeView
 
 		DeleteAllItems();
 
-		BuildFromNodes( _graph.Nodes, true );
-		OnRebuild();
+		if ( _graph is not null )
+		{
+			BuildFromNodes( _graph.Nodes, true );
+			OnRebuild();
+		}
 
 		RestoreViewFromCookie();
 	}
