eee93ad43950929a80e622b6c7e3ff6f365125f8
QuackCola <23345567+QuackCola@users.noreply.github.com>
2026-01-22 14:42:18 -0500

    Create ShaderGraph.Upgrades.cs
    Lets keep ShaderGraph.Serialize somewhat "cleaner" by just dealing with most of the project upgrade stuff in ShaderGraph.Upgrades.cs. For now it has nothing.

---
+++ game/addons/tools/Code/ShaderGraph/ShaderGraph.Upgrades.cs
@@ -0,0 +1,11 @@
+﻿using Editor.NodeEditor;
+using System.Reflection;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace Editor.ShaderGraph;
+
+partial class ShaderGraph
+{ 
+	// TODO
+}
