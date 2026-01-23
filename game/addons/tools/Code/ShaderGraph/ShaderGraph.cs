6a8f4e252c02de5e72abc4678ce1594e4b5ef92f
QuackCola <23345567+QuackCola@users.noreply.github.com>
2026-01-22 22:05:14 -0500

    ShaderGraph.Version++

--- game/addons/tools/Code/ShaderGraph/ShaderGraph.cs
+++ game/addons/tools/Code/ShaderGraph/ShaderGraph.cs
@@ -95,7 +95,7 @@ public sealed partial class ShaderGraph : IGraph
 	public PreviewSettings PreviewSettings { get; set; } = new();
 
 	[Hide]
-	public int Version => 1;
+	public int Version => 2;
 
 	public ShaderGraph()
 	{
