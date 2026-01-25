f4d17f9962da28f51daf2be994ba11c50bb937ac
QuackCola <23345567+QuackCola@users.noreply.github.com>
2026-01-24 23:37:35 -0500

    Some little layout spacing

--- game/editor/ShaderGraph/Code/BlackboardView.cs
+++ game/editor/ShaderGraph/Code/BlackboardView.cs
@@ -63,7 +63,9 @@ public class BlackboardView : Widget
 		canvas.Layout.Spacing = 4;
 
 		var leftColumn = canvas.Layout.AddColumn( 1, false );
+		leftColumn.Spacing = 4;
 		var leftColumnTopLayout = leftColumn.AddRow( 1, false );
+		leftColumnTopLayout.Spacing = 4;
 
 		leftColumnTopLayout.AddStretchCell();
 
