d3f5a7f1946e709cff0dcc53e5f8813ce5d0934c
QuackCola <23345567+QuackCola@users.noreply.github.com>
2026-01-24 19:11:29 -0500

    Initial groundwork for blackboard parameters

---
+++ game/addons/tools/Code/ShaderGraph/BlackboardParameter.cs
@@ -0,0 +1,93 @@
+﻿using Editor.NodeEditor;
+using System.ComponentModel;
+using System.Text.Json.Serialization;
+
+namespace Editor.ShaderGraph;
+
+public interface IBlackboardParameter
+{
+	Guid Identifier { get; }
+
+	string Name { get; }
+}
+
+internal interface IBlackboardParameterType
+{
+	public TypeDescription Type { get; }
+
+	IBlackboardParameter CreateParameter( ShaderGraph graph, string name = "" );
+}
+
+public abstract class BlackboardParameter : IBlackboardParameter
+{
+	[Hide, Browsable( false )]
+	public Guid Identifier { get; set; }
+
+	[JsonIgnore, Hide]
+	public IGraph _graph;
+	[Browsable( false )]
+	[JsonIgnore, Hide]
+	public IGraph Graph
+	{
+		get => _graph;
+		set
+		{
+			_graph = value;
+		}
+	}
+
+	public string Name { get; set; }
+
+
+	public BlackboardParameter()
+	{
+		NewIdentifier();
+		Name = "";
+	}
+
+	public BlackboardParameter( string name ) 
+	{
+		NewIdentifier();
+		Name = name;
+	}
+
+	public Guid NewIdentifier()
+	{
+		Identifier = Guid.NewGuid();
+		return Identifier;
+	}
+
+	internal static IEnumerable<IBlackboardParameterType> GetRelevantParameters( Dictionary<string, IBlackboardParameterType> availableParameters, bool isSubgraph )
+	{
+		return availableParameters.Values.Where( x =>
+		{
+			if ( x is ClassBlackboardParameterType classParameterType )
+			{
+				var targetType = classParameterType.Type.TargetType;
+
+				// TODO : Material input parameters
+
+				// TODO : Subgraph input parameters
+			}
+
+			return true;
+		} );
+	}
+}
+
+public abstract class BlackboardMaterialParameter<T, Y> : BlackboardParameter where Y : IParameterUI
+{
+	[InlineEditor( Label = false ), Group( "Value" )]
+	public T Value { get; set; }
+
+	[InlineEditor( Label = false ), Group( "UI" )]
+	public Y UI { get; set; }
+
+	public bool IsAttribute { get; set; }
+
+	public BlackboardMaterialParameter( string name, T value, bool isAttribute ) : base( name )
+	{
+		Value = value;
+		IsAttribute = isAttribute;
+	}
+}
---
+++ game/addons/tools/Code/ShaderGraph/ClassBlackboardParameterType.cs
@@ -0,0 +1,26 @@
+﻿using Editor.ShaderGraph;
+
+internal class ClassBlackboardParameterType : IBlackboardParameterType
+{
+	public virtual string Identifier => Type.FullName;
+	public TypeDescription Type { get; }
+
+	public ClassBlackboardParameterType( TypeDescription type )
+	{
+		Type = type;
+	}
+
+	public virtual IBlackboardParameter CreateParameter( ShaderGraph graph, string name = "" )
+	{
+		if ( EditorTypeLibrary.Create( Type.Name, Type.TargetType ) is BlackboardParameter parameter )
+		{
+			parameter.Name = name;
+
+			return parameter;
+		}
+		else
+		{
+			throw new Exception( $"Failed to create parameter instance of type \"{Type.Name}\"" );
+		}
+	}
+}
