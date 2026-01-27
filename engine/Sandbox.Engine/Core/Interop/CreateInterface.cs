e36712223279d0f33e83af1fb3de40e38788ea72
SlawekNowy <sosw.slawomir.spiewak@gmail.com>
2026-01-27 17:27:35 +0100

    Format pass

--- engine/Sandbox.Engine/Core/Interop/CreateInterface.cs
+++ engine/Sandbox.Engine/Core/Interop/CreateInterface.cs
@@ -38,7 +38,7 @@ internal static class CreateInterface
 		if ( loadedModules.TryGetValue( dll, out var module ) )
 			return module;
 
-		if ( !NativeLibrary.TryLoad( NetCore.NativeDllPath+nativizedDLL, out module ) )
+		if ( !NativeLibrary.TryLoad( NetCore.NativeDllPath + nativizedDLL, out module ) )
 			return default;
 
 		loadedModules[dll] = module;
