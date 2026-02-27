ebee9c0e022644b9bc87eb5e226795aaec943930
SlawekNowy <sosw.slawomir.spiewak@gmail.com>
2026-02-27 16:25:58 +0100

    Format pass

--- engine/Sandbox.Filesystem/CasefoldSubFilesystem.cs
+++ engine/Sandbox.Filesystem/CasefoldSubFilesystem.cs
@@ -9,10 +9,10 @@ namespace Sandbox;
 
 internal class CasefoldSubFileSystem : SubFileSystem
 {
-    //Is this overkill?
+	//Is this overkill?
 	public CasefoldSubFileSystem( IFileSystem fileSystem, UPath subPath, bool owned = true ) : base( fileSystem, fileSystem.ConvertPathFromInternal( FileSystemStringCaseHelpers.Casepath( fileSystem.ConvertPathToInternal( subPath ) ) ), owned )
 	{
-        
+
 	}
 
 
@@ -23,32 +23,33 @@ internal class CasefoldSubFileSystem : SubFileSystem
 	/// </summary>
 	/// <param name="inPath">path as fed through the parameter</param>
 	/// <returns>the same path with proper casing. This is just partially adjusted if only part of the path exists.</returns>
-	internal  UPath Casepath( UPath inPath )
+	internal UPath Casepath( UPath inPath )
 	{
-		return Casepath_recursive(FallbackSafe,inPath);
+		return Casepath_recursive( FallbackSafe, inPath );
 	}
 
-	private static UPath Casepath_recursive(IFileSystem fs,UPath inPath)
+	private static UPath Casepath_recursive( IFileSystem fs, UPath inPath )
 	{
 		//TODO: We should dive into FallbackSafe until we hit the proper filesystem.
-		if (fs is SubFileSystem sfs)
+		if ( fs is SubFileSystem sfs )
 		{
-			if(sfs.FallbackSafe is SubFileSystem sub_sfs)
+			if ( sfs.FallbackSafe is SubFileSystem sub_sfs )
 			{
-				return Casepath_recursive(sub_sfs,UPath.Combine(sfs.SubPath,inPath));
+				return Casepath_recursive( sub_sfs, UPath.Combine( sfs.SubPath, inPath ) );
 			}
-			
-			if (sfs.FallbackSafe is CasefoldPhysicalFileSystem)
+
+			if ( sfs.FallbackSafe is CasefoldPhysicalFileSystem )
 			{
 				return sfs.FallbackSafe.ConvertPathFromInternal( FileSystemStringCaseHelpers.Casepath( sfs.FallbackSafe.ConvertPathToInternal( inPath ) ) );
 			}
-		} else if (fs is Zio.FileSystems.AggregateFileSystem zafs)
+		}
+		else if ( fs is Zio.FileSystems.AggregateFileSystem zafs )
 		{
 			var entry = zafs.FindFirstFileSystemEntry( inPath );
-			
+
 			if ( entry == null ) return inPath;
 
-			return Casepath_recursive(entry?.FileSystem,inPath);
+			return Casepath_recursive( entry?.FileSystem, inPath );
 			//return entry?.FileSystem.ConvertPathToInternal( entry.Path );
 			//throw new NotImplementedException("not yet implemented.");
 		}
@@ -57,12 +58,12 @@ internal class CasefoldSubFileSystem : SubFileSystem
 
 	protected override UPath ConvertPathFromDelegate( UPath path )
 	{
-		return base.ConvertPathFromDelegate( Casepath(path) );
+		return base.ConvertPathFromDelegate( Casepath( path ) );
 	}
 
 	protected override UPath ConvertPathToDelegate( UPath path )
 	{
-		return base.ConvertPathToDelegate( Casepath(path) );
+		return base.ConvertPathToDelegate( Casepath( path ) );
 	}
 
 
