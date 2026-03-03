514ea20376068fd3ff604c9666a632d3e381e5f4
QuackCola <23345567+QuackCola@users.noreply.github.com>
2026-03-03 16:24:15 -0500

    Dont need the priority on this EditorEvent

--- game/addons/tools/Code/ShaderGraph/ShaderGraph.Upgrades.cs
+++ game/addons/tools/Code/ShaderGraph/ShaderGraph.Upgrades.cs
@@ -158,7 +158,7 @@ internal static class ShaderGraphJsonUpgrader
 	private static (MethodDescription Method, JsonUpgraderAttribute Attribute)[] _methods;
 
 	[EditorEvent.Hotload]
-	[Event( "shadergraph.created", Priority = 100 )]
+	[Event( "shadergraph.created" )]
 	private static void UpdateUpgraders()
 	{
 		_methods = EditorTypeLibrary.GetMethodsWithAttribute<JsonUpgraderAttribute>().ToArray();
