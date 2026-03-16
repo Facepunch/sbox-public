cd326eba703970fb5bc190be823ae126a6c0a2b6
QuackCola <23345567+QuackCola@users.noreply.github.com>
2026-03-16 18:04:30 -0400

    Correct my past change back to Color on the color parameter node
    So that the ColorEditor shows up on the node for the RGBA out :)

--- game/editor/ShaderGraph/Code/Nodes/Parameter.cs
+++ game/editor/ShaderGraph/Code/Nodes/Parameter.cs
@@ -192,7 +192,7 @@ public sealed class Float3 : ParameterNode<Vector3>
 [Title( "Color" ), Category( "Constants" ), Icon( "palette" )]
 public sealed class Float4 : ParameterNode<Color>
 {
-	[Output( typeof( Vector4 ) ), Title( "RGBA" )]
+	[Output( typeof( Color ) ), Title( "RGBA" )]
 	[Hide, Editor( nameof( Value ) )]
 	public NodeResult.Func Result => ( GraphCompiler compiler ) =>
 	{
