e6412ba617e85c0a853c4d354bce128d945c14e7
boxrocket <boxrocket6803@gmail.com>
2026-07-07 13:52:20 -0500

    simplify

--- engine/Sandbox.Engine/Systems/Render/CommandList/CommandList.cs
+++ engine/Sandbox.Engine/Systems/Render/CommandList/CommandList.cs
@@ -486,7 +486,7 @@ public sealed unsafe partial class CommandList
 			}
 			catch ( System.Exception e )
 			{
-				Log.Warning( e, $"Error when executing CommandList {_debugName ?? string.Empty}" );
+				Log.Warning( e, $"Error when executing CommandList {_debugName}" );
 			}
 
 			Graphics.Context.EndPixEvent();
