4bf9fe82a23350bf00b5d75cf5a9f1a29b92b33d
nixx quality <nixx@is-fantabulo.us>
2026-07-23 15:18:16 +0200

    Add a test which makes sure the results from Game.Random are stable forever

---
+++ engine/Tests/Sandbox.Test.Unit/System/StableRandomTest.cs
@@ -0,0 +1,23 @@
+﻿namespace SystemTests;
+
+[TestClass]
+public class StableRandomTest
+{
+	[TestMethod]
+	public void RandomDoesntChange()
+	{
+		Game.SetRandomSeed( 42 );
+
+		Assert.AreEqual( 67, Game.Random.Int( 100 ) );
+		Assert.AreEqual( 14, Game.Random.Int( 100 ) );
+		Assert.AreEqual( 12, Game.Random.Int( 100 ) );
+		Assert.AreEqual( 52, Game.Random.Int( 100 ) );
+		Assert.AreEqual( 17, Game.Random.Int( 100 ) );
+		Assert.AreEqual( 26, Game.Random.Int( 100 ) );
+		Assert.AreEqual( 73, Game.Random.Int( 100 ) );
+		Assert.AreEqual( 51, Game.Random.Int( 100 ) );
+		Assert.AreEqual( 17, Game.Random.Int( 100 ) );
+		Assert.AreEqual( 76, Game.Random.Int( 100 ) );
+		Assert.AreEqual( 23, Game.Random.Int( 100 ) );
+	}
+}
