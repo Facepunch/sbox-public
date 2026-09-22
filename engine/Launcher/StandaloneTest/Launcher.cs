2e83840ab90c6130bd465968e63cf6a3512a2fb8
Arbnor Caravaku <95989314+codenor@users.noreply.github.com>
2026-09-23 08:32:02 +1000

    Set sbox editor launcher vsync param to false to fix AMD gpu launcher flicker

--- engine/Launcher/StandaloneTest/Launcher.cs
+++ engine/Launcher/StandaloneTest/Launcher.cs
@@ -84,7 +84,7 @@ public class PanelLauncherAppSystem : PanelAppSystem
 	{
 		LauncherPreferences.Load();
 
-		window = new Editor.PanelWindow( "Welcome to the s&box editor", new Vector2( 1100, 660 ), null, borderless: true, vsync: true );
+		window = new Editor.PanelWindow( "Welcome to the s&box editor", new Vector2( 1100, 660 ), null, borderless: true, vsync: false );
 		window.MinSize = new Vector2( 880, 540 );
 		window.CanMaximize = false;
 		window.Root.AddChild( new Sandbox.LauncherUI.LauncherWindow( window ) );
